6301710aa3fe3b1d0d030a87a264619d83de8996
xlxs4 <orousoultzoglou@gmail.com>
2023-03-14 21:37:27 +0200

    Add vector operations

--- functional-geometry.rkt
+++ functional-geometry.rkt
@@ -9,3 +9,17 @@
 (define (make-vect x y) (cons x y))
 (define (xcor-vect v) (car v))
 (define (ycor-vect v) (cdr v))
+
+;; vector operations
+(define (transpose v)
+  (make-vect (ycor-vect v) (xcor-vect v)))
+(define (add-vect v w)
+  (make-vect (+ (xcor-vect v) (xcor-vect w))
+             (+ (ycor-vect v) (ycor-vect w))))
+(define (sub-vect v w)
+  (make-vect (- (xcor-vect v) (xcor-vect w))
+             (- (ycor-vect v) (ycor-vect w))))
+
+(define (scale-vect v s)
+  (make-vect (* s (xcor-vect v))
+             (* s (ycor-vect v))))
